471936519303d3eb20b52d219ec31d8dc1bcd90b
Guruprasad Inamdar <guruprasadinamdar@Gurus-MBP-2.local>
2020-04-27 22:34:18 -0400

    Adding async/await on React component, webpack and webpack loaders

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill', './lib/components/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/}
+    ]
+  }
+};
+
+module.exports = config;
